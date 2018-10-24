153f9a8c3c50e48a6f49ee8063bea7b181ccb4f1
suhailiib <40245947+suhailiib@users.noreply.github.com>
2018-10-24 01:44:47 -0500

    Function2

---
+++ function2.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int function2()
+{
+    int x =1000;
+    printf("Here are the most important functions in the universe whoooooooooo %d\n",x);
+    return 0;
+
+}
